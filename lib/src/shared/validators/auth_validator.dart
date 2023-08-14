ed174ea0878ed6dc83b03a89911c8077cada95b4
lucaslopesx <lucas.lopesx1@gmail.com>
2023-08-13 21:04:57 -0300

    Create auth_validator.dart

---
+++ lib/src/shared/validators/auth_validator.dart
@@ -0,0 +1,33 @@
+import 'package:email_validator/email_validator.dart';
+
+class AuthValidator {
+  static String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Por favor, insira um email';
+    }
+    final bool isValid = EmailValidator.validate(value);
+    if (!isValid) {
+      return 'Email inválido';
+    }
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Por favor, insira a senha';
+    }
+
+    if (value.length < 8) {
+      return 'Mínimo de 8 caracteres';
+    }
+
+    return null;
+  }
+
+  static String? validateGeneric(String? value, String type) {
+    if (value == null || value.isEmpty) {
+      return 'Por favor, insira um ${type}';
+    }
+    return null;
+  }
+}
