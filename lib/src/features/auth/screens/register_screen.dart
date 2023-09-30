1744c89e15929675aff11d856a8e98e5ff6cd18e
lucaslopesx <lucas.lopesx1@gmail.com>
2023-09-30 14:20:18 -0300

    fix: register redirect to correct page

--- lib/src/features/auth/screens/register_screen.dart
+++ lib/src/features/auth/screens/register_screen.dart
@@ -5,12 +5,15 @@ import 'package:flutter_frontend_psychology_app/src/features/medical-record/scre
 import 'package:flutter_frontend_psychology_app/src/features/psychologist_search/screens/psychologist_search_screen.dart';
 import 'package:flutter_frontend_psychology_app/src/shared/services/auth/auth_models.dart';
 import 'package:flutter_frontend_psychology_app/src/shared/services/auth/auth_service.dart';
+import 'package:flutter_frontend_psychology_app/src/shared/services/user.service.dart';
 import 'package:flutter_frontend_psychology_app/src/shared/style/input_decoration.dart';
 import 'package:flutter_frontend_psychology_app/src/shared/utils/input_formatter_util.dart.dart';
 import 'package:flutter_frontend_psychology_app/src/shared/utils/user_type.dart';
 import 'package:flutter_frontend_psychology_app/src/shared/validators/auth_validator.dart';
 import 'package:intl/intl.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+import '../../../shared/services/auth/secure_storage_service.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -43,6 +46,10 @@ class _RegisterScreenState extends State<RegisterScreen> {
   AutovalidateMode _autoValidateMode = AutovalidateMode.disabled;
 
   UserType userType = UserType.CLIENT;
+
+  SecureStorageService secureStorageService = SecureStorageService();
+  UserProfileService userProfileService = UserProfileService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
